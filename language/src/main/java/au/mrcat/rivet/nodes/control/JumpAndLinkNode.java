f597982a1832f26bd2e2f5ab7a2f2f71508bf869
Max Guppy <theonly@mrcat.au>
2026-03-22 14:10:54 +1000

    Fix oversight in jalr decoding

--- language/src/main/java/au/mrcat/rivet/nodes/control/JumpAndLinkNode.java
+++ language/src/main/java/au/mrcat/rivet/nodes/control/JumpAndLinkNode.java
@@ -2,19 +2,22 @@ package au.mrcat.rivet.nodes.control;
 
 import au.mrcat.rivet.nodes.RivetNode;
 import au.mrcat.rivet.nodes.RivetOpNode;
+import au.mrcat.rivet.runtime.RiscvJumpException;
 import com.oracle.truffle.api.frame.VirtualFrame;
 
-public class JumpAndLinkNode extends JumpNode {
+public class JumpAndLinkNode extends RivetNode {
+    @Child RivetOpNode targetPc;
     @Child RivetNode link;
 
     public JumpAndLinkNode(RivetOpNode targetPc, RivetNode link) {
-        super(targetPc);
+        this.targetPc = targetPc;
         this.link = link;
     }
 
     @Override
     public void executeVoid(VirtualFrame frame) {
+        long targetPc = this.targetPc.executeLong(frame);
         link.executeVoid(frame);
-        super.executeVoid(frame);
+        throw new RiscvJumpException(targetPc & ~0b1);
     }
 }
