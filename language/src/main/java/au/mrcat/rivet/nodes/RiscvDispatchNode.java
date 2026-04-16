1bc77ac6e4dd06013a1a895a2ce2fb3d62bdb239
Max Guppy <theonly@mrcat.au>
2026-04-17 09:51:34 +1000

    Allow basic blocks to be empty

--- language/src/main/java/au/mrcat/rivet/nodes/RiscvDispatchNode.java
+++ language/src/main/java/au/mrcat/rivet/nodes/RiscvDispatchNode.java
@@ -5,17 +5,21 @@ import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.BlockNode;
 
 public class RiscvDispatchNode extends RivetNode implements BlockNode.ElementExecutor<RivetNode> {
-    @Child BlockNode<RivetNode> instructions;
+    @Child BlockNode<RivetNode> instructions = null;
     private final long nextPc;
 
     public RiscvDispatchNode(RivetNode[] instructions, long nextPc) {
-        this.instructions = BlockNode.create(instructions, this);
+        if (instructions.length != 0) {
+            this.instructions = BlockNode.create(instructions, this);
+        }
         this.nextPc = nextPc;
     }
 
     @Override
     public void executeVoid(VirtualFrame frame) {
-        instructions.executeVoid(frame, BlockNode.NO_ARGUMENT);
+        if (instructions != null) {
+            instructions.executeVoid(frame, BlockNode.NO_ARGUMENT);
+        }
         throw new RiscvJumpException(nextPc);
     }
 
