8454ed7bb49064c8c1b74210c60dbdc083d21fd6
Max Guppy <theonly@mrcat.au>
2026-05-19 09:48:21 +1000

    Disable S mode in riscv-arch-test config

--- vendor/riscv-arch-test/config/rivet/rivet-rv64imac/rvtest_config.svh
+++ vendor/riscv-arch-test/config/rivet/rivet-rv64imac/rvtest_config.svh
@@ -13,13 +13,13 @@
 
 // PMP Grain (G)
 // Set G as needed (e.g., 0, 1, 2, ...)
-`define G 4
+// `define G 4
 
 // Uncomment below if G = 0
 // `define G_IS_0
 
 // PMP mode selection
-`define PMP_16     // Choose between PMP_16 or PMP_64 or None
+// `define None     // Choose between PMP_16 or PMP_64 or None
 
 // Base addresses specific for PMP
 `define RAM_BASE_ADDR       32'h80000000  // PMP Region starts at RAM_BASE_ADDR + LARGEST_PROGRAM
@@ -33,7 +33,6 @@
 `define ZCA_SUPPORTED
 `define ZAAMO_SUPPORTED
 `define ZALRSC_SUPPORTED
-`define SV39_SUPPORTED
 
 `define COUNTINHIBIT_EN_0
 `define COUNTINHIBIT_EN_2
