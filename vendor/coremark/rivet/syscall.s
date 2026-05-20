b26fbdf1a12b558c550f2ab50533537085a1e4a5
Max Guppy <theonly@mrcat.au>
2026-05-20 10:45:27 +1000

    Add rivet barebones port to coremark

---
+++ vendor/coremark/rivet/syscall.s
@@ -0,0 +1,14 @@
+.section .text.init
+.global _start
+_start:
+        la sp, _end
+        jal main
+        li t0, 0x100000
+        li t1, 0x5555
+        sw t1, 0(t0)
+
+.section .text
+.global rv_syscall
+rv_syscall:
+        ecall
+        ret
