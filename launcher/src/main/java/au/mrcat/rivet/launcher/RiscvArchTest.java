b3b313d0e56ac73c33b960ed6bfdbbcfc1da2ba3
Max Guppy <theonly@mrcat.au>
2026-05-31 12:51:28 +1000

    Support multiple banks in riscv-arch-test runner

--- launcher/src/main/java/au/mrcat/rivet/launcher/RiscvArchTest.java
+++ launcher/src/main/java/au/mrcat/rivet/launcher/RiscvArchTest.java
@@ -10,30 +10,24 @@ import java.io.OutputStream;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.TreeMap;
+import java.util.*;
 import java.util.regex.Pattern;
 
 public class RiscvArchTest {
-    static record TestResults(int count, int passed) {
+    record TestResults(int run, int passed) {
     }
 
-    static void main(String[] args) {
-        var riscvTestsDir = System.getenv("RISCV_ARCH_TEST_ELFS");
+    record TestBank(String name, SortedMap<String, ArrayList<Source>> tests) {
+    }
 
-        Pattern extensionPattern;
-        if (args.length > 0) {
-            extensionPattern = Pattern.compile(args[0]);
-        } else {
-            extensionPattern = null;
-        }
+    record BankResults(String name, int totalRun, int totalPassed, SortedMap<String, TestResults> extensions) {
+    }
 
-        var rv64iBasePath = Path.of(riscvTestsDir, "rv64i");
+    static SortedMap<String, ArrayList<Source>> readTestBank(Path bankBase, Pattern extensionPattern) throws IOException {
         var sources = new TreeMap<String, ArrayList<Source>>();
 
-        // $RISCV_ARCH_TEST_ELFS/rv64i contains a bunch of subdirectories, one for each extension
-        try (DirectoryStream<Path> extensionDirectoryStream = Files.newDirectoryStream(rv64iBasePath, (Path p) -> {
+        // $RISCV_ARCH_TEST_ELFS/rv64i contains a bunch of subdirectories, corresponding roughly to extensions
+        try (DirectoryStream<Path> extensionDirectoryStream = Files.newDirectoryStream(bankBase, (Path p) -> {
             if (extensionPattern != null && !extensionPattern.matcher(p.getFileName().toString()).find()) {
                 return false;
             }
@@ -63,15 +57,36 @@ public class RiscvArchTest {
                 // Record the elfs as being from this extension
                 sources.put(extensionPath.getFileName().toString(), extensionSources);
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
 
-        Context context = Context.newBuilder("rv64").out(OutputStream.nullOutputStream()).build();
+        return sources;
+    }
+
+    static SortedMap<String, TestBank> readTests(Path base, Pattern extensionPattern) throws IOException {
+        var banks = new TreeMap<String, TestBank>();
+
+        try (DirectoryStream<Path> bankDirectoryStream = Files.newDirectoryStream(base, (Path p) -> {
+            File f = p.toFile();
+            return f.isDirectory();
+        })) {
+            for (Path bankPath : bankDirectoryStream) {
+                var bankName = bankPath.getFileName().toString();
+                var bankSources = readTestBank(bankPath, extensionPattern);
+                if (bankSources.isEmpty()) {
+                    continue;
+                }
+                banks.put(bankPath.getFileName().toString(), new TestBank(bankName, bankSources));
+            }
+        }
+
+        return banks;
+    }
+
+    static BankResults runBank(Context context, TestBank bank) {
         int totalRun = 0;
         int totalPassed = 0;
         var results = new TreeMap<String, TestResults>();
-        for (var entry : sources.entrySet()) {
+        for (var entry : bank.tests().entrySet()) {
             var extension = entry.getKey();
             var extensionSources = entry.getValue();
 
@@ -103,20 +118,54 @@ public class RiscvArchTest {
             results.put(extension, new TestResults(extensionSources.size(), passed));
             IO.println();
         }
+        return new BankResults(bank.name(), totalRun, totalPassed, results);
+    }
 
-        IO.println(String.format("Test summary: %3d run; %3d passed; %3d failed",
-                totalRun,
-                totalPassed,
-                totalRun - totalPassed));
-        for (var entry : results.entrySet()) {
+    static void formatResults(BankResults results) {
+        IO.println(String.format("Bank %s results: %d run; %d passed; %d failed",
+                results.name(),
+                results.totalRun(),
+                results.totalPassed(),
+                results.totalRun() - results.totalPassed()));
+
+        int firstColWidth = results.extensions().keySet().stream()
+                .map(String::length)
+                .reduce(0, Integer::max);
+        String tableRow = String.format("%%%ds: %%3d run; %%3d passed; %%3d failed", firstColWidth);
+
+        for (var entry : results.extensions().entrySet()) {
             var extension = entry.getKey();
             var extensionResults = entry.getValue();
 
-            IO.println(String.format("%12s: %3d run; %3d passed; %3d failed",
+            IO.println(String.format(tableRow,
                     extension,
-                    extensionResults.count,
-                    extensionResults.passed,
-                    extensionResults.count - extensionResults.passed));
+                    extensionResults.run(),
+                    extensionResults.passed(),
+                    extensionResults.run() - extensionResults.passed()));
+        }
+    }
+
+    static void main(String[] args) throws IOException {
+        var riscvTestsDir = System.getenv("RISCV_ARCH_TEST_ELFS");
+
+        Pattern extensionPattern;
+        if (args.length > 0) {
+            extensionPattern = Pattern.compile(args[0]);
+        } else {
+            extensionPattern = null;
+        }
+
+        var banks = readTests(Path.of(riscvTestsDir), extensionPattern);
+
+        Context context = Context.newBuilder("rv64").out(OutputStream.nullOutputStream()).build();
+        var results = new TreeMap<String, BankResults>();
+        for (TestBank bank : banks.values()) {
+            results.put(bank.name(), runBank(context, bank));
+        }
+
+        for (BankResults bankResults : results.values()) {
+            IO.println();
+            formatResults(bankResults);
         }
     }
 }
